dbcee5d522ac932068ed39d794eee60fbc85f36d
Rachael Passov <github@rachael.io>
2016-06-24 21:23:11 -0400

    renamed angular-dynamic-html to dynamic-bind-html

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,14 @@ app.get('/github', function (req, res) {
 });
 
 /**
- * Routes requests to /angular-dynamic-html to the demo contained in that submodule.
+ * Routes requests to /dynamic-bind-html to the demo contained in that submodule.
  * Sets appropriate static directories to support demo before rendering.
  * This may not be extensible. Remains to be seen. For now, works for the single demo.
  */
-app.get('/angular-dynamic-html', function (req, res) {
-  app.use(express.static('submodules/angular-dynamic-html'));
-  app.use(express.static('submodules/angular-dynamic-html/demo'));
-  res.render('angular-dynamic-html/demo/src/demo.html');
+app.get('/dynamic-bind-html', function (req, res) {
+  app.use(express.static('submodules/dynamic-bind-html'));
+  app.use(express.static('submodules/dynamic-bind-html/demo'));
+  res.render('dynamic-bind-html/demo/src/demo.html');
 });
 
 module.exports = app;
