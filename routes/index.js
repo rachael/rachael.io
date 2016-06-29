28ffb01ab822f12833bb2a9ca88a8e2ca1bff7ca
Rachael Passov <github@rachael.io>
2016-06-29 00:09:45 -0400

    Removing github content after splitting off the github app

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.set('views', [__dirname + '/../views', __dirname + '/../submodules']);
+app.set('views', __dirname + '/../views');
 
 /* GET home page. */
 app.get('/', function(req, res) {
   res.render('index.pug', { title: 'rachael.io' });
-});
-
-app.get('/github', function (req, res) {
-  res.render('github.pug', { title: 'rachael.github.io', theme: 'github-theme' });
-});
-
-/**
- * Routes requests to /dynamic-bind-html to the demo contained in that submodule.
- * Sets appropriate static directories to support demo before rendering.
- * This may not be extensible. Remains to be seen. For now, works for the single demo.
- */
-app.get('/dynamic-bind-html', function (req, res) {
-  app.use(express.static('submodules/dynamic-bind-html'));
-  app.use(express.static('submodules/dynamic-bind-html/demo'));
-  res.render('dynamic-bind-html/demo/src/demo.html');
 });
 
 module.exports = app;
