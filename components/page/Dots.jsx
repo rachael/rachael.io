38ed74d52e0b62fea2085a554a25b5fa5075bc6d
rachael <github@rachael.io>
2020-07-31 17:36:00 -0700

    Randomize transition delay of dots appearing on resize

--- components/page/Dots.jsx
+++ components/page/Dots.jsx
@@ -1,15 +1,32 @@
 import styles from 'styles/page/Dots.module.scss'
 
-// inclusive random integer
+// minimum and maximum number of dots on each side
+const minDots = 6;
+const maxDots = 12;
+// number of dot colors in 'styles/_variables.scss'
+const numDotColors = 8;
+// number of dot widths possible, from Dots css module
+const numDotWidthSteps = 6;
+
+/**
+ * Inclusive random integer
+ * @param  {number} min the min
+ * @param  {number} max the max
+ * @return {number}     a random number between min and max, inclusive
+ */
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Generate one dot, either a 'dot-left' or a 'dot-right'
+ * @param  {string} dotType either 'dot-left' or 'dot-right'
+ * @param  {number} seed    step number of dot generation loop
+ * @return {element}        a Dot element
+ */
 function generateDot(dotType, seed) {
-  const numDotColors = 8;
-  const numDotWidthSteps = 6;
   const dotWidth = getRandomInt(1, numDotWidthSteps);
   return <div
     className={`
@@ -20,15 +37,20 @@ function generateDot(dotType, seed) {
     `}
     style={{
       top: `calc(${getRandomInt(0, 100)}vh - ${dotWidth}rem)`,
-      transform: `translateX(${getRandomInt(-2, 2)}rem)`
+      transform: `translateX(${getRandomInt(-2, 2)}rem)`,
+      transitionDelay: `${getRandomInt(400, 800)}ms`
     }}
   />
 }
 
+/**
+ * Generate an array of Dot elements
+ * @return {element[]} list of dots to display directly in the jsx
+ */
 function generateDots() {
   // generate between 6 and 12 dots on each side
-  const numLeftDots = getRandomInt(6, 12);
-  const numRightDots = getRandomInt(6, 12);
+  const numLeftDots = getRandomInt(minDots, maxDots);
+  const numRightDots = getRandomInt(minDots, maxDots);
 
   const dots = [];
 
@@ -45,7 +67,11 @@ function generateDots() {
   return dots;
 }
 
-function Dots({ text }) {
+/**
+ * Displays randomly generated decorative dots along the left and right edge of
+ * the main container.
+ */
+function Dots() {
   return (
     <div className={styles.dots}>
       {generateDots()}
