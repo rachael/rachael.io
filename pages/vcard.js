db4dda553d7f9636e071b47e246935f6adfb8a0e
Rachael <github@rachael.io>
2020-08-11 14:18:27 -0700

    Revert vcard redirect so window's location does not change

--- pages/vcard.js
+++ pages/vcard.js
@@ -1,10 +1,28 @@
-import { StaticFileRedirect } from 'components/page';
+import { useRouter } from 'next/router'
 
-function VCard() {
-  return StaticFileRedirect({
-    staticFileUrl: '/Rachael Passov.vcf',
-    renderComponent: null, // TODO: render vcard placeholder
-  })
+/**
+ * Static redirect for vcard file. Does not use StaticFileRedirect as there is
+ * nothing to render; cannot use HOC/abstract as the key functionality is the
+ * return statement directly from the functional component. This causes the
+ * vcard file to start downloading without changing the window's location from
+ * the user's perspective.
+ */
+function VCard({ ctx }) {
+  const router = useRouter()
+  // Make sure we're in the browser
+  if (typeof window !== 'undefined') {
+    router.push('/Rachael Passov.vcf');
+    return;
+  }
+}
+
+VCard.getInitialProps = ctx => {
+  // We check for ctx.res to make sure we're on the server.
+  if (ctx.res) {
+    ctx.res.writeHead(302, { Location: '/Rachael Passov.vcf' });
+    ctx.res.end();
+  }
+  return { };
 }
 
 export default VCard
