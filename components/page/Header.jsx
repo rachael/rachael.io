65e0ba221dc34782e9bae05bad02397d22b5fdd8
Rachael <github@rachael.io>
2020-08-14 17:40:13 -0700

    Serve fonts locally

--- components/page/Header.jsx
+++ components/page/Header.jsx
@@ -5,6 +5,8 @@ function Header({ title }) {
     <Head>
       <title>rachael.io{ title && ` :: ${title}` }</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preload" href="/fonts/Montserrat-Regular.woff2" />
+      <link rel="preload" href="/fonts/Rowdies-Bold.woff2" />
     </Head>
   )
 }
