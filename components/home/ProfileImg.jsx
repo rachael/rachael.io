5d0f40c5b504f787a31a3c8117d521d19c4cb558
Rachael <github@rachael.io>
2020-08-18 10:36:54 -0700

    Change profile image to <img> with alt text for accessibility

--- components/home/ProfileImg.jsx
+++ components/home/ProfileImg.jsx
@@ -2,7 +2,9 @@ import styles from 'styles/home/Profile.module.scss'
 
 function ProfileImg() {
   return (
-    <div className={styles['profile-img']} />
+    <div className={styles['profile-img']}>
+      <img src="/profile_sm.png" alt="Profile picture" />
+    </div>
   )
 }
 
