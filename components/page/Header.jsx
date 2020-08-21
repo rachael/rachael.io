dc18a8d52907fff6b41861448a246bf5bc883264
Rachael <github@rachael.io>
2020-08-21 12:46:15 -0700

    Update Header preload/prefetch

--- components/page/Header.jsx
+++ components/page/Header.jsx
@@ -6,10 +6,11 @@ function Header({ title }) {
       <title>rachael.io{ title && ` :: ${title}` }</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preload" href="/fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-      <link rel="preload" href="/fonts/Rowdies-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" href="/fonts/Rowdies-Light.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       <link rel="preload" href="/fonts/Sacramento.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       <link rel="preload" href="/profile_sm.png" as="image" />
       <link rel="preload" href="/bg_postits_blur.png" as="image" media="(min-width: 800px)" />
+      <link rel="prefetch" href="/profile_sm_blur.png" as="image" />
     </Head>
   )
 }
