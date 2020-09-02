f16b1712bf379a6d6e1e4accb13207816daa0c14
Rachael <github@rachael.io>
2020-09-02 12:21:53 -0700

    Fix comments in Header

--- components/page/Header.jsx
+++ components/page/Header.jsx
@@ -4,14 +4,14 @@ function Header({ title }) {
   return (
     <Head>
       <title>rachael.io{ title && ` :: ${title}` }</title>
-      // icons
+      {/* icons */}
       <link rel="icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#2b5797" />
       <meta name="theme-color" content="#ffffff" />
-      // preload/prefetch
+      {/* // preload/prefetch */}
       <link rel="preload" href="/fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       <link rel="preload" href="/fonts/Rowdies-Light.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       <link rel="preload" href="/fonts/Sacramento.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
