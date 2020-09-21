4e61df8f5490ff7898c72230b93e0d030517a612
Rachael <github@rachael.io>
2020-09-21 12:14:20 -0700

    Remove old ProfileName file

--- components/profile/index.js
+++ components/profile/index.js
@@ -1,13 +1,11 @@
 import Profile from './Profile'
 import ProfileButton from './ProfileButton'
 import ProfileImg from './ProfileImg'
-import ProfileName from './ProfileName'
 import ProfileText from './ProfileText'
 
 export {
   Profile,
   ProfileButton,
   ProfileImg,
-  ProfileName,
   ProfileText,
 }
